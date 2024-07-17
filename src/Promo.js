edaeb21782b8c6ec212993cfd88c5882105fbf24
qcywxb <163746096+qcwhyy@users.noreply.github.com>
2024-07-17 23:24:36 +0300

    Update Promo.js

--- src/Promo.js
+++ src/Promo.js
@@ -18,14 +18,26 @@ function Promo() {
             <header className="App-header">
                 <div className="fullscreen-bg">
                     <div className="overlay">
+                        {/*<a className='page' href="#"><h1>HEARTBREAKER HEARTBREAKER HEARTBREAKER</h1></a>*/}
                     </div>
 
 
                     <video src={breakerad} autoPlay loop muted className="fullscreen-bg__video">
                     </video>
                 </div>
+                {/*<h1>HEARTBREAKER</h1>>*/}
             </header>
             <main>
+                {/*    <div className="product-list">*/}
+                {/*    {products.map(product => (*/}
+                {/*            <div className="product" key={product.id}>*/}
+                {/*                <img src={product.image} alt={product.name} />*/}
+                {/*        <h3>{product.name}</h3>*/}
+                {/*        <p>{product.price}&#8381;</p>*/}
+                {/*        <button>Добавить в корзину</button>*/}
+                {/*      </div>*/}
+                {/*  ))}*/}
+                {/*</div>*/}
 
 
                 <div className="main-container">
